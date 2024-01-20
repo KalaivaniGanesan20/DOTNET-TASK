69bd2a12d515f6b68fcc1b390c3975fc6ef14aaf
KalaivaniGanesan20 <156308012+KalaivaniGanesan20@users.noreply.github.com>
2024-01-20 12:55:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace IDisposableExample
+{
+    public class Program
+    {
+        static void Main()
+        {
+             var printcsv=new PrintCSV();
+            try
+            {
+              printcsv.printcsv();
+            }
+            finally
+            {
+              ((IDisposable)printcsv).Dispose();
+            }
+        }
+    }
+    public class PrintQuote : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("I am disposing the print object");
+        }
+    }
+    public class PrintCSV:PrintQuote
+    {
+        public new void Dispose()
+        {
+            Console.WriteLine("I am disposing CSv object");
+        }
+        public void  printcsv()
+        {
+            Console.WriteLine("Printing csv file");
+        }
+    }
+}
